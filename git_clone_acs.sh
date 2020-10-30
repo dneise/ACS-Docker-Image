666b89629112acff6f9f95fa081ef98947d57b23
Dominik Neise <neised@phys.ethz.ch>
2020-10-30 08:43:40 +0100

    download boost manually as long as the git-lfs is broken

--- git_clone_acs.sh
+++ git_clone_acs.sh
@@ -13,4 +13,10 @@ set -u # aborts automatically if we try to expand an undefined variable
 
 git clone https://bitbucket.alma.cl/scm/asw/acs.git ./acs
 cd ./acs
-git checkout acs/$ACS_TAG
+git checkout acs/$ACS_TAG || true
+
+cd ExtProd/PRODUCTS
+rm -f boost_1_66_0.tar.bz2
+curl -L https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2 \
+     --output boost_1_66_0.tar.bz2
+
